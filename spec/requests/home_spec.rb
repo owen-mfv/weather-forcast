133684868f84f627c962b46e5881d5dd8817d3a4
owen <le.vinh.khang@moneyforward.vn>
2022-06-26 22:43:20 +0700

    update UT

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,12 +2,22 @@ require 'rails_helper'
 
 RSpec.describe "Homes", type: :request do
   describe "GET /index" do
-    before do
-      VCR.use_cassette "GET current weather data" do
-        get "/"
+    context "when using gem `vcr`", skip: false do
+      before do
+        VCR.use_cassette "GET current weather data" do
+          get "/"
+        end
       end
+
+      it { expect(response.status).to eq 200 }
     end
 
-    it { expect(response.status).to eq 200 }
+    context "when not using gem `vcr`", skip: true do
+      before do
+        get "/"
+      end
+
+      it { expect(response.status).to eq 200 }
+    end
   end
 end
